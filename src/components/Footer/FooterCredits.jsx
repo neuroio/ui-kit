1f895ee6bb2408545b74d274e331fdd9488fed46
Dmitriy Balashov <mockedcat@gmail.com>
2020-05-15 11:53:07 +0300

    Footer: text styles changed

--- src/components/Footer/FooterCredits.jsx
+++ src/components/Footer/FooterCredits.jsx
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 
+import { colors } from "../../style/color";
+
 const FooterCredits = styled.div`
+  font-size: 14px;
   line-height: 18px;
-  font-size: 15px;
-  font-weight: 300;
   text-align: right;
-  margin-left: auto;
+  color: ${colors.slate};
 `;
 
 export { FooterCredits };
--- src/components/Footer/FooterDisclaimer.jsx
+++ src/components/Footer/FooterDisclaimer.jsx
@@ -1,18 +1,19 @@
 import styled from "styled-components";
 
+import { colors } from "../../style/color";
+
 const FooterDisclaimer = styled.div`
+  font-weight: normal;
   font-size: 13px;
   line-height: 18px;
-  font-weight: 300;
-  color: #223b55;
-  width: 500px;
-  text-align: left;
+  color: ${colors.grayMedium};
+  width: 430px;
 
   p {
     margin-top: 0;
 
     &:not(:last-child) {
-      margin-bottom: 6px;
+      margin-bottom: 12px;
     }
   }
 `;
