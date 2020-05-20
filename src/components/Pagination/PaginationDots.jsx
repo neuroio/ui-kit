9d834818952178b98ab647bf93f2c34fada27656
Dmitriy Balashov <mockedcat@gmail.com>
2020-05-20 16:51:12 +0300

    Pagination: legacy component was removed

--- src/components/Pagination/PaginationDots.jsx
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-const PaginationDots = styled.span`
-  margin-right: 8px;
-`;
-
-export { PaginationDots };
--- src/components/Pagination/StyledPagination.jsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const StyledPagination = styled.nav`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`;
-
-export { StyledPagination };
