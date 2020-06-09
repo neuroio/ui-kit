8c1b45ca8328ae2792ba7d8cc32f1ed848e33db7
Dmitriy Balashov <mockedcat@gmail.com>
2020-06-09 18:01:38 +0300

    ButtonUpload: missed className, data testid props were added

--- src/components/ButtonUpload/index.jsx
+++ src/components/ButtonUpload/index.jsx
@@ -1,21 +1,35 @@
 import React from "react";
+import styled from "styled-components";
 import PropTypes from "prop-types";
 
 import { useRef } from "react";
 
 import { Button } from "../Button";
 
-function ButtonUpload({ onUpload, children, theme }) {
+function ButtonUpload({
+  onUpload,
+  children,
+  theme,
+  className,
+  "data-testid": testId,
+}) {
   const inputRef = useRef(null);
 
   return (
     <>
-      <input ref={inputRef} type="file" onChange={onUpload} hidden />
+      <input
+        ref={inputRef}
+        type="file"
+        onChange={onUpload}
+        data-testid={testId}
+        hidden
+      />
       <Button
         theme={theme}
         onClick={(ev) => {
           inputRef.current.click(ev);
         }}
+        className={className}
       >
         {children}
       </Button>
@@ -23,14 +37,19 @@ function ButtonUpload({ onUpload, children, theme }) {
   );
 }
 
+const StyledButtonUpload = styled(ButtonUpload)``;
+
 ButtonUpload.propTypes = {
   theme: PropTypes.string,
   children: PropTypes.oneOfType([PropTypes.node, PropTypes.array]).isRequired,
   onUpload: PropTypes.func.isRequired,
+  className: PropTypes.string,
+  "data-testid": PropTypes.string,
 };
 
 ButtonUpload.defaultProps = {
   theme: "light",
+  "data-testid": "button-upload",
 };
 
-export { ButtonUpload };
+export { ButtonUpload, StyledButtonUpload };
