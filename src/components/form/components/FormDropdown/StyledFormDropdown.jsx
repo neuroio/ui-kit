32814d56151fa58ff4e4a5c5c911ca39eb7cbdc1
Dmitriy Balashov <mockedcat@gmail.com>
2020-06-11 13:53:26 +0300

    FormDropdown: loading styles changed

--- src/components/form/components/FormDropdown/StyledFormDropdown.jsx
+++ src/components/form/components/FormDropdown/StyledFormDropdown.jsx
@@ -1,15 +1,22 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
 const StyledFormDropdown = styled.div`
   position: relative;
   width: ${({ width }) => (width ? `${width}px` : "100%")};
   opacity: ${({ disabled }) => (disabled ? 0.5 : 1)};
 
-  ${({ isFetching }) =>
-    isFetching &&
-    css`
-      opacity: 0.8;
-    `}
+  &:after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    opacity: ${({ isFetching }) => (isFetching ? 1 : 0)};
+    visibility: ${({ isFetching }) => (isFetching ? "visible" : "hidden")};
+    transition: opacity 200ms ease-in-out, visibility 200ms ease-in-out;
+    background-color: rgba(255, 255, 255, 0.6);
+  }
 `;
 
 export { StyledFormDropdown };
