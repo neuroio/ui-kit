4f03cf1735dab0133a1c2715b03542a0038ed216
Dmitriy Balashov <mockedcat@gmail.com>
2020-06-16 17:48:14 +0300

    PageWrapper: Container component added to set wrapper width

--- src/components/PageWrapper/index.jsx
+++ src/components/PageWrapper/index.jsx
@@ -2,15 +2,18 @@ import styled from "styled-components";
 
 const PageWrapper = styled.div`
   box-sizing: border-box;
-  width: 1000px;
   min-height: 100vh;
-  margin: 0 auto;
   position: relative;
-  padding-left: 30px;
-  padding-right: 30px;
   padding-bottom: 90px;
   display: flex;
   flex-direction: column;
+`;
+
+PageWrapper.Container = styled.div`
+  width: 1000px;
+  padding-left: 30px;
+  padding-right: 30px;
+  margin: 0 auto;
 `;
 
 export { PageWrapper };
