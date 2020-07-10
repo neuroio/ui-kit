59dd11e3d2e8e003e0d1fffa3243a6c36908250f
Dmitriy Balashov <mockedcat@gmail.com>
2020-07-10 14:09:14 +0300

    useRefRect: now updates on interval for keeping rect actual

--- src/hooks/use-ref-rect.js
+++ src/hooks/use-ref-rect.js
@@ -1,7 +1,22 @@
 import { useEffect, useState, useLayoutEffect } from "react";
+import { useInterval } from "react-use";
 
 function useRefRect(ref, onChange) {
   const [rect, setRect] = useState(null);
+  function updateNodeRect(node) {
+    const newRect = node.getBoundingClientRect();
+
+    if (JSON.stringify(newRect) !== JSON.stringify(rect)) {
+      setRect(node.getBoundingClientRect());
+    }
+  }
+
+  useInterval(() => {
+    if (ref.current) {
+      updateNodeRect(ref.current);
+    }
+  }, 100);
+
   useEffect(() => {
     if (onChange) {
       onChange(rect);
@@ -10,11 +25,7 @@ function useRefRect(ref, onChange) {
 
   useLayoutEffect(() => {
     if (ref.current) {
-      const newRect = ref.current.getBoundingClientRect();
-
-      if (JSON.stringify(newRect) !== JSON.stringify(rect)) {
-        setRect(ref.current.getBoundingClientRect());
-      }
+      updateNodeRect(ref.current);
     }
   });
 
