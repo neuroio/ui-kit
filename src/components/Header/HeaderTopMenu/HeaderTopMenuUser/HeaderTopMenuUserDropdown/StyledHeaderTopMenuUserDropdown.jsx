00d79e2831ae18e2bd1a6943bf1d7f7e0f766d8c
Dmitriy Balashov <mockedcat@gmail.com>
2020-08-04 14:17:00 +0300

    HeaderTopMenuUserDropdown: styles changed

--- src/components/Header/HeaderTopMenu/HeaderTopMenuUser/HeaderTopMenuUserDropdown/StyledHeaderTopMenuUserDropdown.jsx
+++ src/components/Header/HeaderTopMenu/HeaderTopMenuUser/HeaderTopMenuUserDropdown/StyledHeaderTopMenuUserDropdown.jsx
@@ -7,7 +7,8 @@ import { colors } from "../../../../../style";
 const StyledHeaderTopMenuUserDropdown = styled(PopupContainer)`
   margin: 0;
   background-color: ${colors.whiteSimple};
-  width: 280px;
+  min-width: 280px;
+  max-width: 348px;
   box-shadow: 0px 0px 80px rgba(162, 182, 189, 0.2);
   border-radius: 4px;
   padding: 32px 32px;
