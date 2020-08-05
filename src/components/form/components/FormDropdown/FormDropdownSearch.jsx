6cd2796b267292b1f1957a21c6456606a20587bb
Dmitriy Balashov <mockedcat@gmail.com>
2020-08-05 16:57:51 +0300

    FormDropdownSearch: width now is auto

--- src/components/form/components/FormDropdown/FormDropdownSearch.jsx
+++ src/components/form/components/FormDropdown/FormDropdownSearch.jsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 import { Search } from "../../../Search";
 
 const FormDropdownSearch = styled(Search.Compact)`
-  width: 260px;
   margin-bottom: 8px;
   margin-left: 24px;
   margin-right: 24px;
