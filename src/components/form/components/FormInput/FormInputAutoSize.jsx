144dbc4b06108c4a9242145981569ee26e2ff1e3
Dmitriy Balashov <mockedcat@gmail.com>
2020-09-09 11:30:23 +0300

    FormInputAutoSize now removes all html tags onPaste

--- src/components/form/components/FormInput/FormInputAutoSize.jsx
+++ src/components/form/components/FormInput/FormInputAutoSize.jsx
@@ -26,6 +26,11 @@ const FormInputAutoSize = styled(StyledFormInput).attrs(
     onChange: addNameToTarget(name, onChange),
     onBlur: addNameToTarget(name, onBlur),
     onFocus: addNameToTarget(name, onFocus),
+    onPaste(evt) {
+      evt.preventDefault();
+      const text = evt.clipboardData.getData("text/plain");
+      document.execCommand("insertHTML", false, text);
+    },
     "data-testid": getTestId(name, testId),
   })
 )`
