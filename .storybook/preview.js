1b157c08348b653689ca26f20151f5e2f58ce6cf
Dmitriy Balashov <mockedcat@gmail.com>
2021-02-02 15:18:13 +0300

    layout components stories updated

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,24 @@
 import { GlobalStyles, I18nProvider } from "../src/components";
 import { Normalize } from "styled-normalize";
+import { MemoryRouter } from "react-router";
 
 const withProvider = (Story, context) => (
-  <I18nProvider>
-    <div style={{ padding: 16, maxWidth: 940 }}>
-      <Normalize />
-      <GlobalStyles />
-      <Story {...context} />
-    </div>
-  </I18nProvider>
+  <MemoryRouter>
+    <I18nProvider>
+      <div
+        style={{
+          padding: 16,
+          maxWidth: 940,
+          minHeight: "100vh",
+          position: "relative",
+        }}
+      >
+        <Normalize />
+        <GlobalStyles />
+        <Story {...context} />
+      </div>
+    </I18nProvider>
+  </MemoryRouter>
 );
 
 export const decorators = [withProvider];
